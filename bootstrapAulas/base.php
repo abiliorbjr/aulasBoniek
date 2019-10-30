0ae82f8cf002af63138564653468dabf6876b5c5
Abilio Junior <abiliorbjr@gmail.com>
2019-10-30 07:21:46 -0300

    foi inserido imagens

---
+++ bootstrapAulas/base.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<title>CRUD</title>
+	<style type="text/css">
+		.row [class^=col-], .row .col{
+			background-color: #DDD;
+			border: 1px solid #000;
+		}
+		.row{
+			background-color: #FF2298;
+			padding: 10px;
+		}
+	</style>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<link rel="stylesheet" href="../bootstrap4/css/bootstrap.min.css"/>
+	
+</head>
+<body>
+	<!-- .divnomeDaClasse -->
+	 <div class="container" style="background-color: #6495ED">
+	 	<div>Obrigado meu Deus!</div>	
+	</div>
+	<!-- esse script poderia ser colocado no meta, mas aqui ele carrega mais rápido -->
+	<script type="text/javascript" src="../bootstrap4/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="../bootstrap4/js/jquery.min.js"></script>
+	<script type="text/javascript" src="../bootstrap4/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
---
+++ bootstrapAulas/imagens.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<title>CRUD</title>
+	<style type="text/css">
+		.row [class^=col-], .row .col{
+			background-color: #DDD;
+			border: 1px solid #000;
+		}
+		.row{
+			background-color: #FF2298;
+			padding: 10px;
+		}
+	</style>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<link rel="stylesheet" href="../bootstrap4/css/bootstrap.min.css"/>
+	
+</head>
+<body>
+	<!-- .divnomeDaClasse -->
+	 <div class="container" style="background-color: #6495ED">
+	 	<!-- <img src="imagens/amiguinhos-de-Deus.png" alt=""> --><!-- se a pasta estiver dentro da pasta dos arquivos -->
+	 	<img src="../imagens/amiguinhos-de-Deus.png" alt="imagem" class="img-fluid">
+<div class="row justify-content-center">
+	 	<div style="width: 500px">
+	 		<img src="../imagens/amiguinhos-de-Deus.png" alt="imagem" class="img-fluid" >
+	 	</div>
+
+	 	
+</div>
+
+	 	<div class="container">
+	 		<div style="width:300px">
+	 			<img src="../imagens/IMG_5393.jpg"  class="rounded img-fluid float-right" alt="pais">
+	 		</div>
+		</div>	
+
+	</div>
+	<!-- esse script poderia ser colocado no meta, mas aqui ele carrega mais rápido -->
+	<script type="text/javascript" src="../bootstrap4/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="../bootstrap4/js/jquery.min.js"></script>
+	<script type="text/javascript" src="../bootstrap4/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
